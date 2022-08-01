e01295771cdf6af643e3a1f02be2502e2e60c3a6
David Sousa <david01sousa@Hotmail.com>
2022-08-01 19:20:39 -0300

    SUPER ATT NA ABA DE PROJETOS

--- src/components/portifolio/Portifolio.jsx
+++ src/components/portifolio/Portifolio.jsx
@@ -1,38 +1,27 @@
-import React from 'react'
+import React from "react"
+import "./Portifolio.css"
+import Card from "./Card"
+import Portfolio_data from "./PortifolioData"
 
-import './Portifolio.css'
-import PortifolioData from './PortifolioData.js'
-
-export const Portifolio = () => {
+const Portfolio = () => {
   return (
     <>
-      <section id='Portifolio' className='content-portfolio'>
-        <div className="heading">
-          <h1>MEUS PROJETOS</h1>
-          <h3>Um pouco mais dos meus melhores projetos</h3>
+      <section className='Portfolio top' id='portfolio'>
+        <div className='container'>
+          <div className='heading text-center '>
+            <h4>AQUI ESTÃO ALGUNS DOS MEUS PROJETOS</h4>
+            <h1>Meu Portifólio</h1>
+          </div>
+
+          <div className='content grid'>
+            {Portfolio_data.map((value, index) => {
+              return <Card key={index} image={value.image} category={value.category} totalLike={value.totalLike} title={value.title} link={value.link} />
+            })}
+          </div>
         </div>
-        <div className='content'>
-            {PortifolioData.map((val) =>{
-              return(
-                <>
-                  <div className='box'>
-                    <div className='img'>
-                      <img src={val.cover} alt="" />
-                    </div>
-                    <div className='overlay'>
-                      <div className='text'>
-                        <h2>{val.title}</h2>
-                        <p>{val.desc}</p>
-                      </div>
-                    </div>
-                  </div>      
-                </>
-              )
-            })}    
-        </div>                         
       </section>
     </>
   )
 }
 
-export default Portifolio;
+export default Portfolio
--- src/components/portifolio/PortifolioData.js
+++ src/components/portifolio/PortifolioData.js
@@ -1,40 +1,46 @@
-const Data = [
-    {
-      id: 1,
-      title: "Amazon Copia",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      cover: "./images/4602f2f5153477027a754713438bf3d0.jpg",
-    },
-    {
-      id: 2,
-      title: "Digital Marketing",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      cover: "./images/work2.jpg",
-    },
-    {
-      id: 3,
-      title: "Digital Marketing",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      cover: "./images/work3.jpg",
-    },
-    {
-      id: 4,
-      title: "Digital Marketing",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      cover: "./images/work4.jpg",
-    },
-    {
-      id: 5,
-      title: "Digital Marketing",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      cover: "./images/work5.jpg",
-    },
-    {
-      id: 6,
-      title: "Digital Marketing",
-      desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-      cover: "./images/work6.jpg",
-    },
-  ]
-  
-  export default Data
+const Portfolio_data = [
+  {
+    id: 1,
+    category: "Amazon Copia",
+    totalLike: "600",
+    title: "saiba mais sobre a copia da amazon",
+    image: "./images/4602f2f5153477027a754713438bf3d0.jpg",
+    link: "https://amazon-umber.vercel.app/",
+  },
+  {
+    id: 2,
+    category: "APPLICATION",
+    totalLike: "750",
+    title: "Mobile app landing design & maintain",
+    image: "./image/portfolio-02.jpg",
+  },
+  {
+    id: 3,
+    category: "PHOTOSHOP",
+    totalLike: "630",
+    title: "Logo design creativity & Application ",
+    image: "./image/portfolio-03.jpg",
+  },
+  {
+    id: 4,
+    category: "FIGMA",
+    totalLike: "360",
+    title: "Mobile app landing design & Services",
+    image: "./image/portfolio-04.jpg",
+  },
+  {
+    id: 5,
+    category: "WEB DESIGN",
+    totalLike: "280",
+    title: "Design for tecnology & services",
+    image: "./image/portfolio-05.jpg",
+  },
+  {
+    id: 6,
+    category: "WEB DESIGN",
+    totalLike: "690",
+    title: "App for tecnology & services",
+    image: "./image/portfolio-06.jpg",
+  },
+]
+export default Portfolio_data
